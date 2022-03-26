1b5232b614bc3725d22e6836f3ffb06e19c988d5
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-03-26 17:11:56 +0530

    made some initial function for LeetcodeTrello
    Co-authored-by: divyanshu <cdivyanshu7@gmail.com>

---
+++ leetcode_trello.py
@@ -0,0 +1,75 @@
+from trello import TrelloClient
+
+class LeetcodeTrello(TrelloClient):
+
+  board_name = "Leetcode"
+
+  def __init__(self, api_key, api_secret=None, token=None, token_secret=None):
+    super().__init__(api_key, api_secret, token, token_secret)
+    self.board = self.create_or_get_board()
+
+
+
+
+  def create_or_get_board(self):
+    """
+    create or get board
+    """
+    print("Creating or getting board")
+    boards = self.list_boards()
+    for board in boards:
+      if board.name == LeetcodeTrello.board_name:
+        question_list = list(map(lambda x:x.name, board.all_lists()))
+        print(question_list)
+        if "Questions" not in question_list:
+          question_list = board.add_list('Questions')
+        return board
+
+
+
+    board = self.add_board(LeetcodeTrello.board_name)
+    self.clear_board(board.id)
+    self.add_list("Questions", pos=0)
+
+    return board
+
+
+
+
+
+  def clear_board(self, board_id):
+    """
+    Clears all cards from a board.
+    """
+    board = self.get_board(board_id)
+    labels = board.get_labels()
+    for label in labels:
+      board.delete_label(label.id)
+
+
+    lists = board.open_lists()
+    for list_ in lists:
+      list_.close()
+
+
+  def add_question(self, question):
+
+    question_list = self.board.all_lists()[-1]
+    question_list.add_card(question)
+
+  def mark_question(self,card_id, label):
+    question_list = self.board.all_lists()[-1]
+
+    question_list.list_cards(query={label: [label]})
+    pass
+
+  def get_all_questions(self, label):
+
+    self.board.list_cards()
+    pass
+
+  def get_all_member_mails(self):
+    return [i.email for i in self.board.all_members().fetch()]
+
+  def add_label(self, mail):
+    self.board.add_label(mail)
--- trello_app.py
+++ trello_app.py
@@ -1,13 +1,21 @@
 from os import getenv
-from trello import TrelloClient
+from leetcode_trello import LeetcodeTrello
 
-client = TrelloClient(
+client = LeetcodeTrello(
     api_key=getenv("TRELLO_API_KEY"),
     api_secret=getenv("TRELLO_API_SECRET"),
-    token=getenv("TRELLO_API_TOKEN"),
+    token=getenv("TRELLO_API_TOKEN")
     # token_secret=''
 )
 
-all_boards = client.list_boards()
-last_board = all_boards
-print(last_board.name)
+
+
+# all_boards = client.list_boards()[1]
+
+
+# question_list = all_boards[1].all_lists()
+
+# for i in question_list:
+#     print(i.list_cards())
+
+# client.clear_board(all_boards[1].id)
